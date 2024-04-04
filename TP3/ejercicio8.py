89b2c32fbee3853304cf56ba2499c05afb56f9ad
Ivan Higa <ivan.higa@globallogic.com>
2024-04-04 15:06:26 -0300

    Resolucion ejercicio 8

--- TP3/ejercicio8.py
+++ TP3/ejercicio8.py
@@ -4,3 +4,19 @@ Un año es bisiesto cuando es divisible por 4.
 Sin embargo, aquellos años que sean divisibles por 4 y también por 100 no son bisiestos, a menos que también sean divisibles por 400. 
 Por ejemplo, 1900 no fue bisiesto pero sí el 2000.
 '''
+
+year = int(input('Ingresar año a evaluar\n'))
+
+mensajeBisiesto = 'El año ' + str(year) + ' es bisiesto'
+mensajeNoBisiesto = 'El año ' + str(year) + ' no es bisiesto'
+
+if year%4 == 0:
+    if year %100 == 0:
+        if year % 400 == 0:
+            print(mensajeBisiesto)
+        else:
+            print(mensajeNoBisiesto)     
+    else:
+        print(mensajeBisiesto)
+else:
+    print(mensajeNoBisiesto)
