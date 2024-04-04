c13a72ae9e5ad25a70f5a1cee06542c814eb1541
Ivan Higa <ivan.higa@globallogic.com>
2024-04-04 14:48:52 -0300

    Resolucion ejercicio 6

--- TP3/ejercicio6.py
+++ TP3/ejercicio6.py
@@ -4,3 +4,19 @@ rústica. Si el número de páginas supera las 300 la encuadernación debe ser e
 páginas sobrepasa las 600 se hace necesario un procedimiento especial de encuadernación que incrementa el costo en otros $3360. Desarrollar un programa
 que calcule el costo de un libro dado el número de páginas.
 '''
+
+cantPaginas = int(input('Ingrese la cantidad de paginas\n'))
+precioBase = 5000
+costoEncuadernacionTela = 1200
+costoMayorA600 = 3360
+precioPorPagina = 32
+precioFinal = precioBase
+
+precioFinal = precioBase + cantPaginas * precioPorPagina
+
+if cantPaginas > 300 :
+    precioFinal = precioFinal + costoEncuadernacionTela
+    if cantPaginas > 600:
+        precioFinal = precioFinal + costoMayorA600
+
+print('El precio final para un libro de', cantPaginas, 'paginas es $', precioFinal)
