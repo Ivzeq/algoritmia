1dae0d5f3b2ccc0e1382845af09e358e78333faa
Ivan Higa <ivan.higa@globallogic.com>
2024-04-04 19:13:02 -0300

    Planteo del ejercicio 10

--- TP3/ejercicio10.py
+++ TP3/ejercicio10.py
@@ -3,3 +3,12 @@ Diseñar un programa que calcule y muestre el sueldo neto de un empleado en base
 el sueldo en 5% del salario bruto por cada año de antigüedad, mientras que si es casado se le incrementa el sueldo en 7% del bruto por cada año de antigüedad.
 También se le realizan los siguientes descuentos: Jubilación: 11%, Obra Social: 3%, Sindicato: 3%
 '''
+
+sueldoBruto = int(input('Ingrese el sueldo bruto'))
+antiguedad = int(input('Ingrese la antiguedad en años'))
+
+porcentajeJubilacion = 11
+porcentajeObraSocial = 3
+porcentajeSindicato = 3
+porcentajeSoltero = 5
+porcentajeCasado = 7
