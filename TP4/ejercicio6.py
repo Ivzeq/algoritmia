97b744f2106716dbf2918d908c041811a70d6a32
Ivan Higa <ivzeq.higa@gmail.com>
2024-04-08 19:59:30 -0300

    Resolucion ejercicio 6

--- TP4/ejercicio6.py
+++ TP4/ejercicio6.py
@@ -2,3 +2,12 @@
 Mostrar la tabla de multiplicar (entre 1 y 12) del número 4. ¿Cómo cambiaría el
 algoritmo para que el usuario pueda decidir la tabla de multiplicar a mostrar?
 '''
+
+factor = 1
+
+resultado = 0
+
+while (factor <= 12):
+    resultado = 4 * factor
+    print('4 x', factor, '=', resultado)
+    factor += 1
