4243d98bf7e86c609426eda0aafba09210535de8
Ivan Higa <ivan.higa@globallogic.com>
2024-04-08 17:04:21 -0300

    Enunciados TP4

---
+++ TP4/ejercicio11.py
@@ -0,0 +1,5 @@
+'''
+Realizar un programa que lea un número natural H e imprima un mensaje indicando
+si H es primo o no. Se dice que un número es primo cuando sólo es divisible
+por sí mismo y por la unidad.
+'''
