2d3e4adf54a8daaaa3c3411f6abdf4bc585826f9
Ivan Higa <ivzeq.higa@gmail.com>
2024-04-09 20:56:19 -0300

    Resolucion ejercicio 5

--- TP4/ejercicio5.py
+++ TP4/ejercicio5.py
@@ -2,3 +2,12 @@
 - Ejercicio 5: Desarrollar un programa que imprima los números naturales comprendidos entre
 1 y N. El valor de N se ingresa desde el teclado.
 '''
+numeroImprimir = 1
+numeroNaturalN = 0
+while numeroNaturalN <= 0:
+    numeroNaturalN = int(input('Ingrese un numero natural\n'))
+
+
+while numeroImprimir <= numeroNaturalN:
+    print(numeroImprimir)
+    numeroImprimir += 1
