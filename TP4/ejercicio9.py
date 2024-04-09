bf580a1acc82287c2bfd9868d8ed347b840f742c
Ivan Higa <ivzeq.higa@gmail.com>
2024-04-08 21:02:32 -0300

    Resolucion ejercicio 9

--- TP4/ejercicio9.py
+++ TP4/ejercicio9.py
@@ -4,3 +4,16 @@ cuántos con numeración impar en un día. Escribir un programa que permita ingr
 la terminación de la patente (entre 0 y 9) hasta ingresar -1 e informe
 cuántos vehículos pasaron con numeración par y cuántos con numeración impar.
 '''
+
+ultimoDigitoPatente = int(input('Ingrese el ultimo digito de la patente\n'))
+nPatentesPares = 0
+nPatentesImpares = 0
+
+while (ultimoDigitoPatente != -1):
+    if ultimoDigitoPatente %2 == 0:
+        nPatentesPares += 1
+    elif ultimoDigitoPatente %2 != 0:
+        nPatentesImpares += 1
+    ultimoDigitoPatente = int(input('Ingrese el ultimo digito de la patente\n'))
+    
+print('El numero de patentes pares es', nPatentesPares, 'y el nummero de impares es', nPatentesImpares)
