984b8466edef4f80d7a32ef7084c5002060ed6c6
Ivan Higa <ivzeq.higa@gmail.com>
2024-04-08 21:25:30 -0300

    Resolucion ejercicio 11

--- TP4/ejercicio11.py
+++ TP4/ejercicio11.py
@@ -3,3 +3,20 @@ Realizar un programa que lea un número natural H e imprima un mensaje indicando
 si H es primo o no. Se dice que un número es primo cuando sólo es divisible
 por sí mismo y por la unidad.
 '''
+
+numero = int(input('Ingresar numero a evaluar\n'))
+contadorNumero = 2
+divisibilidadCorrecta = True
+
+while contadorNumero < numero:
+    if numero % contadorNumero !=0:
+        contadorNumero += 1
+    elif numero % contadorNumero == 0:
+        divisibilidadCorrecta = False
+        contadorNumero = numero + 1
+        
+
+if divisibilidadCorrecta == True:
+    print('El numero', numero, 'es primo')
+else:
+    print('El numero', numero, 'no es primo')
