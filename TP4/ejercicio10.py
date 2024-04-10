22c3cda089440ebc08670341f593522b5748816a
Ivan Higa <ivzeq.higa@gmail.com>
2024-04-09 21:16:22 -0300

    Resolucion ejercicio 10

--- TP4/ejercicio10.py
+++ TP4/ejercicio10.py
@@ -4,3 +4,18 @@ de todos los enteros X tales que 0 < X <= N. Desarrollar un programa para calcul
 el factorial de un número dado. Deberán rechazarse las entradas inválidas
 (menores que 0).
 '''
+
+factorialN = int(input('Ingrese un numero entero a evaluar como factorial \n'))
+if factorialN <0:
+    while factorialN <0:
+        factorialN = int(input('Ingrese un numero entero a evaluar como factorial \n'))
+
+factorActual = 1
+resultadoFactorial = 1
+
+
+while factorActual <= factorialN:
+    resultadoFactorial *= factorActual
+    factorActual += 1
+
+print('El factorial de', factorialN, 'es', resultadoFactorial)
