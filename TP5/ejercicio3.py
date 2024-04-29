27ecb5f4d9693b6642eeb36105f40a0d3c44ef37
Ivan Higa <ivzeq.higa@gmail.com>
2024-04-29 20:41:37 -0300

    done exercise 3

--- TP5/ejercicio3.py
+++ TP5/ejercicio3.py
@@ -14,3 +14,43 @@ El finde carga se determina ingresando -1 como cantidad solicitada. Al finalizar
 · Cantidad de ventas en las que SOLO se aplicó el precio base, es decir que no se le realizaron descuentos.
 '''
 
+precioBase = 0
+nProducto = 0
+
+nVentas = 0
+valorTotalVentas = 0
+precioPrmedio = 0
+nVentas10percent = 0
+nVentasPrecioBase = 0
+
+nProducto = int(input('Ingrese la cantidad de producto\n'))
+
+while (nProducto != -1):
+    while (nProducto <= 0):
+        nProducto = int(input('Ingrese la cantidad de producto\n'))
+    if(nProducto> 0):
+        precioBase = int(input('Ingrese el precio base del producto\n'))
+
+    if (nProducto <= 12):
+        nVentasPrecioBase += nProducto
+        valorTotalVentas = nProducto * precioBase
+    elif(nProducto > 12):
+        nVentasPrecioBase += 12
+        valorTotalVentas +=  12 * precioBase
+        if (nProducto > 12 and nProducto <= 100):
+            nVentas10percent += nProducto - 12
+            valorTotalVentas += (nProducto - 12) * (precioBase * 0.9)
+        elif (nProducto > 100):
+            nVentas10percent += 88
+            valorTotalVentas += 88 * (precioBase * 0.9) + (nProducto - 100) * (precioBase * 0.75)
+            
+
+    nVentas += nProducto
+    precioPrmedio = valorTotalVentas / nVentas
+    nProducto = int(input('Ingrese la cantidad de producto\n'))
+
+print('El valor total de la venta es:', valorTotalVentas)
+print('El precio promedio por unidad es:', precioPrmedio)
+print('La cantidad total de ventas es:', nVentas)
+print('La cantidad de ventas con un 10% de descuento es:', nVentas10percent)
+print('La cantidad de ventas a precio base es:', nVentasPrecioBase)
