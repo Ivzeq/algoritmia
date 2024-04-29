bf923ad5d6cb3dd784bd8c5b82953dedba57ec5f
Ivan Higa <ivan.higa@globallogic.com>
2024-04-29 15:21:02 -0300

    resolucion ejercicio 3

--- TP6/ejercicio3.py
+++ TP6/ejercicio3.py
@@ -2,3 +2,12 @@
 Imprimir una columna de asteriscos, donde su altura se recibe como parámetro.
 '''
 
+nFilas = 0
+contadorCiclo = 0
+
+while (nFilas <= 0):
+    nFilas = int(input('Ingrese el numero de filas deseado\n'))
+
+while(contadorCiclo < nFilas):
+    print('*')
+    contadorCiclo += 1
