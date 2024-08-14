5c21da7b43d19449188c9eabf208f6790fe6aebf
Ivan Higa <ivzeq.higa@gmail.com>
2024-08-14 20:14:57 -0300

    update algo

--- Algo 1/C2/Matrices/ejercicio1.py
+++ Algo 1/C2/Matrices/ejercicio1.py
@@ -3,12 +3,11 @@ Crea un programa que pida un número al usuario un número de mes (por ejemplo, 
 Debes usar una matriz para su parametrización y una función para la recuperación del dato.
 '''
 def getCantidadDias (mes, matriz):
-    for fila in (0, len(matriz)):
+    for fila in range(0, len(matriz)):
         if matriz[fila][0] == mes:
-            return matriz [fila] [1]
+            return matriz [fila][1]
 
-matriz_mex_dia = [[1, 31],[2, 28],[3, 31],[4, 30],[5, 31],[6, 30],[7, 31],[8, 31],[9, 30],[10, 31],[11, 30],[12, 31]]
+matriz=[[1,31],[2,28],[3,31],[4,30],[5,31],[6,30],[7,31],[8,31],[9,30],[10,31],[11,30],[12,31]]
 numeroDeMes = int(input('Ingrese mes\n'))
 
-
-print(getCantidadDias(numeroDeMes, matriz_mex_dia))
+print(getCantidadDias(numeroDeMes, matriz))
